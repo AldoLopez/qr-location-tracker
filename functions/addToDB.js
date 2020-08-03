5b2933c8d3ae1db0e8a4122da9624d2cb0639953
Aldo Lopez <lopez.aldo.a@gmail.com>
2020-08-03 12:01:39 -0400

    add function

--- functions/addToDB.js
+++ functions/addToDB.js
@@ -1,6 +1,19 @@
+const { FAUNA_KEY } = process.env;
+const faunadb = require('faunadb'),
+  q = faunadb.query;
+
+const client = new faunadb.Client({ secret: FAUNA_KEY });
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: 'Hello, World',
-  };
+  const [deviceId, location] = { ...event.queryStringParameters };
+  const date = new Date().toString();
+  client
+    .query(
+      q.Create(q.Collection('devices'), { data: { deviceId, location, date } })
+    )
+    .then((data) => ({
+      statusCode: 200,
+      body: `Added device: ${deviceId}`,
+    }))
+    .catch((err) => console.log(err));
 };
