85971439e60085d1b261fb51e811759146b0875d
Aldo Lopez <lopez.aldo.a@gmail.com>
2020-08-03 17:38:23 -0400

    add qr logic - test

--- functions/getFromDB.js
+++ functions/getFromDB.js
@@ -6,14 +6,14 @@ const client = new faunadb.Client({
   secret: FAUNA_KEY,
 });
 
-exports.handler = async (event, context) => {
-    const deviceId = event.queryStringParameters.deviceId;
-    const location = event.queryStringParameters.location;
-    const date = new Date().toString();
-    const data = { deviceId, location, date };
-    console.log(data);
-    dg client
-      .query(q.Create(q.Collection('devices'), { data }))
-      .then((ret) => console.log(ret))
-      .catch((err) => console.log(err));
-  };
+// exports.handler = async (event, context) => {
+//     const deviceId = event.queryStringParameters.deviceId;
+//     const location = event.queryStringParameters.location;
+//     const date = new Date().toString();
+//     const data = { deviceId, location, date };
+//     console.log(data);
+//     dg client
+//       .query(q.Create(q.Collection('devices'), { data }))
+//       .then((ret) => console.log(ret))
+//       .catch((err) => console.log(err));
+//   };
