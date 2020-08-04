58dd85c85bb3ff1c9ec243c091bd53b06997b6fb
Aldo Lopez <lopez.aldo.a@gmail.com>
2020-08-04 08:39:25 -0400

    logs

--- functions/getFromDB.js
+++ functions/getFromDB.js
@@ -8,7 +8,7 @@ const client = new faunadb.Client({
 
 exports.handler = async (event, context) => {
   const { identity, user } = context.clientContext;
-  console.log(`identity: ${identity}`);
+  console.log(`identity: ${JSON.stringify(identity)}`);
   console.log(`User: ${user}`);
   if (!user) {
     return { statusCode: 403, body: 'You shall not pass!' };
