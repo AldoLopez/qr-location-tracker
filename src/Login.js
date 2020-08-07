72bde835c0c2dee0f9474013515f7b0ec252997e
Aldo Lopez <lopez.aldo.a@gmail.com>
2020-08-07 08:37:16 -0400

    change messaging

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import netlifyIdentity from 'netlify-identity-widget';
 
-const login = () => netlifyIdentity.open();
 const Login = () => (
   <div>
     <p>Login to access</p>
